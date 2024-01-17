9245ca1d5d0b1fcd9a08a822a471e41c10a07464
GayathriShankar003 <138317099+GayathriShankar003@users.noreply.github.com>
2024-01-17 05:31:34 +0000

    files modified

--- main.py
+++ main.py
@@ -1,125 +1,105 @@
 from flask import Flask, render_template, request, session
-from replit import db
 import requests
+from replit import db
+import os
 
 app = Flask(__name__)
-app.secret_key = 'BAD_SECRET_KEYY'
 
-app.config["SESSION_COOKIE_SAMESITE"] = 'None'
-app.config["SESSION_COOKIE_SECURE"] = True
+app.secret_key = os.environ['flask_secret_key']
 
-if 'users' not in db:
-  db['users'] = []
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config['SESSION_COOKIE_SECURE'] = True
 
-if 'dogs_generated' not in db:
-  db['dogs_generated'] = 0
+if 'total_dogs_generated' not in db:
+  db['total_dogs_generated'] = 0
 
 if 'last_dog' not in db:
   db['last_dog'] = ''
 
-if 'leaderboard' not in db:
-  db['leaderboard'] = []
+if 'users' not in db:
+  db['users'] = []
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
-  
+def home():
   if request.method == 'POST':
+    print('You clicked the Login/Signup button!')
     user_name = request.form['user_name']
-    user = create_or_update_user(user_name)
-    # user = get_user_from_database(user_name)
     session['user'] = user_name
+    user = create_or_update_user(user_name)
   else:
     user = None
-  
-  return render_template('index.html', 
-                         dogs_generated=db['dog_images_generated'],
-                         user=user,
-                         leaderboard=enumerate(db['leaderboard']),
-                        )
+    
+  return render_template('index.html',
+                        dogs_generated=db['total_dogs_generated'],
+                        dog_image=db['last_dog'],
+                        users=enumerate(get_leaderboard(db['users'])),
+                        user=user)
 
 @app.route('/get_dog')
 def get_dog():
+  response = requests.get('https://dog.ceo/api/breeds/image/random')
+
+  data = response.json()
+
+  dog_image = data['message']
+
+  print(db['total_dogs_generated'])
+
+  db['last_dog'] = dog_image
+
   if session['user']:
     user = get_user_from_database(session['user'])
-    user['dogs_generated'] = user.get('dogs_generated', 0) + 1
-  
-  db['dog_images_generated'] = db.get('dog_images_generated', 0) + 1
-  
-  response = requests.get("https://dog.ceo/api/breeds/image/random")
-  data = response.json()
-  
-  db['last_dog'] = data['message']
-  dogs_generated=db['dog_images_generated']
-  dog_image=data['message']
+    user['dogs_generated'] += 1
 
-  db['leaderboard'] = get_leaderboard(db['users'])
-    
-  return render_template('index.html', 
-                         dogs_generated=db['dog_images_generated'],
-                         dog_image=data['message'],
-                         user=user,
-                         leaderboard=enumerate(db['leaderboard']),
-                        )
+  db['total_dogs_generated'] += 1
+  return render_template('index.html',
+                        dog_image=dog_image,
+                        user=user,
+                        users=enumerate(get_leaderboard(db['users'])),
+                        dogs_generated=db['total_dogs_generated'])
+
 
 @app.route('/logout')
 def logout():
   session['user'] = None
-  return render_template('index.html', 
-                         dogs_generated=db['dog_images_generated'],
-                         leaderboard=enumerate(db['leaderboard']),
-                        )
+  return render_template('index.html',
+                        dogs_generated=db['total_dogs_generated'])
 
-'''
-Create new user or update their logins by 1 if they don't exist
-Takes in 2 arguments, the actual user object and their username
->>> create_or_update_user(None, 'david')
-ObservedDict(value={'user_name': 'david', 'logins': 1, 'dogs_generated': 0})
 
-Update existing user
->>> create_or_update_user(ObservedDict(value={'user_name': 'kevin', 'logins': 3, 'dogs_generated': 0}), 'kevin')
-ObservedDict(value={'user_name': 'kevin', 'logins': 3, 'dogs_generated': 0})
-'''
 def create_or_update_user(user_name):
-  user = get_user_from_database(user_name)  
+  user = get_user_from_database(user_name)
+
   if user:
-      user['logins'] += 1
+    print('USER ALREADY EXISTS')
+    user['logins'] += 1
+    print(user)
+      
+
   else:
-    print('errors are happening here.')
+    # new user
+    print('NEW USER 🎉!')
     db['users'].append({
       'user_name': user_name,
       'logins': 1,
-      'dogs_generated': 0,
+      'dogs_generated': 0
     })
     user = get_user_from_database(user_name)
-
+    
   return user
 
-# abstract this away from users
-'''
-If a user exists...
->>> get_user_from_database('david')
-ObservedDict(value={'user_name': 'david', 'logins': 15, 'dogs_generated': 0})
+# DRY - DO-NOT REPEAT YOURSELF
 
-If no user...
->>> get_user_from_database('kevin')
-None
-'''
 def get_user_from_database(user_name):
-  user = [user for user in db['users'] if user['user_name'] == user_name]
-  return user[0] if user else None
+   user = [user for user in db['users'] if user['user_name'] == user_name]
+   return user[0] if user else None
 
-
-'''
->>> get_leaderboard()
-'''
 def get_leaderboard(users):
-  # Example of how to use `sorted`
-  # sorted(companies, key=lambda company: company[2], reverse=True))
-  leaderboard = sorted([user.value for user in users], 
-                     key=lambda user: user['dogs_generated'], 
-                     reverse=True)
+  return sorted(db['users'], key=lambda user: user['dogs_generated'], reverse=True)
 
-  return leaderboard
+@app.template_filter()
+def trophy_or_position(index):
+  return '1 🏆' if index == 0 else index + 1
+  
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=81)
